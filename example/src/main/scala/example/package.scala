ab92d2efe07177de89742bc093edb1c258ef14cf
Jed Wesley-Smith <jed@atlassian.com>
2014-06-02 15:11:20 +1000

    added Range and some more REPL easy living

--- example/src/main/scala/example/package.scala
+++ example/src/main/scala/example/package.scala
@@ -1,6 +1,23 @@
-package object example {
-  implicit def ScalaFunctionToJava[A, B](f: (A => B)) =
+trait LowPriorityConversions {
+  implicit def ToJavaFunction[A, B](f: (A => B)) =
     new java.util.function.Function[A, B] {
       def apply(a: A) = f(a)
+    }
+
+  implicit def ToJavaBiFunction[A, B, C](f: ((A, B) => C)) =
+    new java.util.function.BiFunction[A, B, C] {
+      def apply(a: A, b: B) = f(a, b)
+    }
+}
+
+package object example extends LowPriorityConversions {
+  implicit def ToJavaConsumer[A](f: (A => Unit)) =
+    new java.util.function.Consumer[A] {
+      def accept(a: A) = f(a)
+    }
+
+  implicit def ToJavaBinaryOperator[A](f: ((A, A) => A)) =
+    new java.util.function.BinaryOperator[A] {
+      def apply(a: A, b: A) = f(a, b)
     }
 }
