2b31b337494a89b0d9f45c3e0918bc7591c178ec
Jed Wesley-Smith <jed@atlassian.com>
2014-06-12 14:27:02 +1000

    implement everything in terms of fold

--- example/src/main/java/example/five/Option.java
+++ example/src/main/java/example/five/Option.java
@@ -1,30 +1,37 @@
 package example.five;
 
+import static java.util.Objects.requireNonNull;
+import static java.util.function.Function.identity;
+
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public abstract class Option<T> implements Iterable<T>, Foldable<T> {
   public static <T> Option<T> unit(T t) {
-    return new Some<>(t);
+    return new Some<>(requireNonNull(t));
   }
 
   public static <T> Option<T> none() {
     return new None<>();
   }
 
-  public abstract <R> Option<R> map(Function<T, R> f);
+  /**
+   * Catamorphism or interpreter for Option algebra.
+   * @param s the supplier of the something when there is nothing
+   * @param f the function that computes what to do with the value
+   * @return the right thing
+   */
+  public abstract <R> R fold(Supplier<R> s, Function<T, R> f);
 
-  public abstract <R> Option<R> flatMap(Function<T, Option<R>> f);
+  //
+  // implementations
+  //
 
-  public abstract boolean isDefined();
-
-  public abstract T get();
-
-  public abstract T getOrElse(T otherwise);
-  
   static class Some<T> extends Option<T> {
     final T value;
 
@@ -32,74 +39,63 @@ public abstract class Option<T> implements Iterable<T>, Foldable<T> {
       this.value = value;
     }
 
-    @Override public <R> Option<R> map(Function<T, R> f) {
-      return new Some<R>(f.apply(value));
-    }
-
-    @Override public <R> Option<R> flatMap(Function<T, Option<R>> f) {
+    @Override public <R> R fold(Supplier<R> s, Function<T, R> f) {
       return f.apply(value);
-    }
-
-    @Override public boolean isDefined() {
-      return true;
-    }
-
-    @Override public T get() {
-      return value;
-    }
-
-    @Override public T getOrElse(T otherwise) {
-      return value;
-    }
-
-    @Override public <A> A foldRight(A zero, BiFunction<T, A, A> f) {
-      return f.apply(value, zero);
-    }
-
-    @Override public Iterator<T> iterator() {
-      return Collections.singleton(value).iterator();
-    }
-
-    @Override public String toString() {
-      return "Some(" + value + ")";
     }
   }
 
   static class None<T> extends Option<T> {
     None() {}
 
-    @Override public <R> Option<R> map(Function<T, R> f) {
-      return none();
+    @Override public <R> R fold(Supplier<R> s, Function<T, R> f) {
+      return s.get();
     }
+  }
 
-    @Override public <R> Option<R> flatMap(Function<T, Option<R>> f) {
-      return new None<R>();
-    }
+  //
+  // usage
+  //
 
-    @Override public boolean isDefined() {
-      return false;
-    }
+  public final boolean isDefined() {
+    return fold(() -> false, t -> true);
+  }
 
-    @Override public T get() {
+  public final T getOrElse(T otherwise) {
+    return fold(() -> otherwise, identity());
+  }
+
+  public final T get() throws NoSuchElementException {
+    return fold(() -> {
       throw new NoSuchElementException();
-    }
+    }, identity());
+  }
 
-    @Override public T getOrElse(T otherwise) {
-      return otherwise;
-    }
+  public final <R> Option<R> map(Function<T, R> f) {
+    // cannot use unit as it checks for null
+    return fold(() -> none(), t -> new Some<>(f.apply(t))); 
+  }
 
-    @Override public Iterator<T> iterator() {
-      return Collections.emptyIterator();
-    }
+  public final <R> Option<R> flatMap(Function<T, Option<R>> f) {
+    return fold(() -> none(), t -> f.apply(t));
+  }
 
-    @Override public <A> A foldRight(A zero, BiFunction<T, A, A> f) {
-      return zero;
-    }
+  public final Option<T> filter(Predicate<T> p) {
+    return fold(() -> none(), t -> p.test(t) ? this : none());
+  }
 
+  public final boolean exists(Predicate<T> p) {
+    return fold(() -> false, t -> p.test(t));
+  }
 
-    @Override public String toString() {
+  @Override public final <A> A foldRight(A zero, BiFunction<T, A, A> f) {
+    return fold(() -> zero, t -> f.apply(t, zero));
+  }
 
-      return "None()";
-    }
+  @Override public Iterator<T> iterator() {
+    return fold(() -> Collections.emptyIterator(), t -> Collections.singleton(t).iterator());
+  }
+
+  @Override public String toString() {
+    return fold(() -> "None()", t -> "Some(" + t + ")");
   }
 }
